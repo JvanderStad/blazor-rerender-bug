ba16d53cacb80b9bf8b6e97b62e50cdb37e4c881
Jeffrey van der Stad <jeffrey@hybridsaas.com>
2020-04-02 13:44:52 +1100

    Removed more code

--- Components/Date/Date.razor.DisplayValue.cs
+++ Components/Date/Date.razor.DisplayValue.cs
@@ -12,54 +12,39 @@ namespace BlazorBug
 		public string DisplayValue
 		{
 			get => _displayValue;
-			set => SetDisplayValue(value);
+			set
+			{
+				if (value == _displayValue)
+					return;
+
+				Logger.LogWarning("SetDisplayValue: {value}", value);
+
+				// Can we parse the text value into a DateTime?
+				try
+				{
+					var parsed = DateTime.Parse(value);
+
+					DisplayValueChangedInvoke(parsed.ToShortDateString());
+
+					// Change DateTime _value to parsed value and update bindings
+					ValueChangedInvoke(parsed);
+				}
+				catch (Exception)
+				{
+					Logger.LogError("Could not parse value {value}", value);
+
+					// Could not parse
+					// Continue to update _displayValue to the unparseble text value
+					DisplayValueChangedInvoke(value);
+
+					// Set the Value to null and update bindings
+					ValueChangedInvoke(default);
+				}
+			}
 		}
 
 		[Parameter]
 		public EventCallback<string> DisplayValueChanged { get; set; }
-
-		/// <summary>
-		/// DisplayValue property setter
-		/// </summary>
-		/// <param name="value"></param>
-		public void SetDisplayValue(string value)
-		{
-			if (value == _displayValue)
-				return;
-
-			Logger.LogWarning("SetDisplayValue: {value}", value);
-
-			// Can we parse the text value into a DateTime?
-			var parsed = ParseDisplayValue(value);
-			if (parsed.InputControlError != null)
-			{
-				Logger.LogError("Could not parse value {value}", value);
-
-				// Could not parse
-				// Continue to update _displayValue to the unparseble text value
-				DisplayValueChangedInvoke(value);
-				
-				// Set the Value to null and update bindings
-				ValueChangedInvoke(default);
-
-				// Set the error
-				SetError(parsed.InputControlError);
-				
-				// Notify error state has changed
-				HasErrorsChangedInvoke();
-				return;
-			}
-
-			// Change _displayvalue to the parsed value and update bindings
-			DisplayValueChangedInvoke(parsed.DisplayValue);
-			
-			// Change DateTime _value to parsed value and update bindings
-			ValueChangedInvoke(parsed.Value);
-
-			// Notify error state has changed
-			if (ClearError())
-				HasErrorsChangedInvoke();
-		}
 
 		/// <summary>
 		/// Update _displayValue and update bindings
@@ -70,57 +55,6 @@ namespace BlazorBug
 			_displayValue = displayValue;
 			Logger.LogWarning("Set _displayValue to '{_displayValue}' and update bindings", _displayValue);
 			DisplayValueChanged.InvokeAsync(_displayValue);
-		}
-
-		/// <summary>
-		/// Parse the user input
-		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
-		public virtual ParseDisplayValueResult ParseDisplayValue(string value)
-		{
-			if (String.IsNullOrEmpty(value))
-			{
-				return new ParseDisplayValueResult
-				{
-					DisplayValue = "",
-					Value = default
-				};
-			}
-
-			try
-			{
-				var parsed = DateTime.Parse(value);
-				return new ParseDisplayValueResult
-				{
-					DisplayValue = GetDisplayValue(parsed),
-					Value = parsed
-				};
-			}
-			catch (Exception exception)
-			{
-				return new ParseDisplayValueResult
-				{
-					InputControlError = new InputControlError
-					{
-						Message = $"Error parsing value '{value}'",
-						Details = exception.Message,
-					}
-				};
-			}
-		}
-
-		/// <summary>
-		/// Convert the value into a display value
-		/// </summary>
-		/// <param name="value"></param>
-		/// <returns></returns>
-		public virtual string GetDisplayValue(DateTime? value)
-		{
-			if (value == null)
-				return null;
-
-			return value.Value.ToShortDateString();
 		}
 	}
 }
--- Components/Date/Date.razor.Value.cs
+++ Components/Date/Date.razor.Value.cs
@@ -7,36 +7,31 @@ namespace BlazorBug
 	public partial class Date
     {
 		private DateTime? _value { get; set; }
+
 		[Parameter]
 		public DateTime? Value
 		{
 			get => _value;
-			set => SetValue(value);
+			set
+			{
+				if (value == _value)
+					return;
+
+				Logger.LogWarning("SetValue: {value}", value);
+
+				DisplayValueChangedInvoke(value?.ToShortDateString());
+				ValueChangedInvoke(value);
+			}
 		}
 
 		[Parameter]
 		public EventCallback<DateTime?> ValueChanged { get; set; }
-
 
 		private void ValueChangedInvoke(DateTime? value)
 		{
 			_value = value;
 			Logger.LogWarning("Set _value to '{_value}' and update bindings", _value);
 			ValueChanged.InvokeAsync(_value);
-		}
-
-		public void SetValue(DateTime? value)
-		{
-			if (value == _value)
-				return;
-
-			Logger.LogWarning("SetValue: {value}", value);
-
-			DisplayValueChangedInvoke(GetDisplayValue(value));
-			ValueChangedInvoke(value);
-
-			if (ClearError())
-				HasErrorsChangedInvoke();
 		}
 	}
 }
